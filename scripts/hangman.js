fcebcd7226938536cd26b4f95477b481c6f4ad0a
Ella <93593284+efranxes@users.noreply.github.com>
2021-12-09 21:07:40 +0000

    Update hangman.js
    Add guessing the letters

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,1 +1,41 @@
-console.log("bonjour bestie :)");
+//console.log("bonjour bestie :)");
+
+let word = "orange";
+
+let allLetters = document.querySelectorAll('.alphabetContainer .letter');
+//console.log(allLetters);
+let guessContainer = document.querySelector('.guessContainer');
+
+let guessLetterHTML = '<div class = "guessLetter"></div>';
+for (counter = 0; counter < word.length; counter ++){
+    guessContainer.insertAdjacentHTML('beforeend', guessLetterHTML );
+}
+
+let guessLetters = document.querySelectorAll('.guessLetter')
+
+allLetters.forEach(function(letter){
+    //console.log(letter);
+    letter.addEventListener('click', function(event){
+        let currentLetterHTML = event.target;
+        if(!currentLetterHTML.classList.contains('clicked')){
+            let currentLetter = event.target.innerHTML.toLowerCase();
+            currentLetterHTML.classList.add('clicked');
+            //console.log(currentLetter);
+            let answers = [...word.matchAll(currentLetter)];
+            console.log(answers);
+            if(answers){
+                answers.forEach(function(answer){
+                    //console.log(answer.index);
+                    for(counter = 0; counter < guessLetters.length; counter ++){
+                        if(counter == answer.index){
+                            guessLetters[answer.index].innerHTML = currentLetter.toUpperCase();
+                        }
+                    }
+                })
+            } else {
+                //show next part of hangman
+            }
+        }
+    })
+})
+
